3d01226b8645692b983991cef32777c758e3ac01
YogeshSahu0 <yogesh.sahu@opensenselabs.com>
2023-03-26 20:14:53 +0530

    getting signup page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Signup from "./components/Signup";
+import Header from "./components/Header";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  // const notify = () => toast.success("Wow so easy!");
+  // const notifyError = () => toast.error("password not matched")
   return (
     <div className="App">
-     <h1>Expense tracker</h1>
+      <Header />
+      <Signup />
+      <ToastContainer/>
     </div>
   );
 }
